fafe4a5976e18603a31505c1d00741a4e5197c20
Antonio Hernández Ambrocio <antonio.hdza12@gmail.com>
2023-01-03 17:50:12 -0600

    Initial Commit

---
+++ SimpleMath/SimpleMath.h
@@ -0,0 +1,18 @@
+//
+//  SimpleMath.h
+//  SimpleMath
+//
+//  Created by Antonio Hernandez Ambrocio on 03/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleMath.
+FOUNDATION_EXPORT double SimpleMathVersionNumber;
+
+//! Project version string for SimpleMath.
+FOUNDATION_EXPORT const unsigned char SimpleMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleMath/PublicHeader.h>
+
+
